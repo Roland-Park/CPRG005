b5c83f6baeaf878b3755512c3054c68461a277b5
Ro <rolandpark.rp@gmail.com>
2021-05-20 18:47:09 -0600

    Create login and signup forms Add logo Add default data to db

--- CPRG005.Final.Data/MarinaDbContext.cs
+++ CPRG005.Final.Data/MarinaDbContext.cs
@@ -20,8 +20,14 @@ namespace CPRG005.Final.Data
 
             modelBuilder.Entity<LeaseType>().HasData(
                 new LeaseType() { Id = 1, Name = "Daily", StandardRateAmount = 20.00m },
+                new LeaseType() { Id = 1, Name = "Weekly", StandardRateAmount = 80.00m },
                 new LeaseType() { Id = 2, Name = "Monthly", StandardRateAmount = 200.00m },
                 new LeaseType() { Id = 3, Name = "Yearly", StandardRateAmount = 2000.00m }
+                );
+
+            modelBuilder.Entity<Location>().HasData(
+                new Location() { Id = 1, Name = "Inland Lake" },
+                new Location() { Id = 2, Name = "San Diego" }
                 );
         }
     }
